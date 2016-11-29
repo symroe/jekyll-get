0a37e9ee0e0dba0e5d7d5572f90e2ac8ad2f7a70
Sym Roe <sym.roe@talusdesign.co.uk>
2016-11-29 19:20:29 +0000

    Load cached files rather than remote json

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -16,23 +16,18 @@ module Jekyll_Get
         config = [config]
       end
       config.each do |d|
-        begin
-          target = site.data[d['data']]
+        data_source = (site.config['data_source'] || '_data')
+        path = "#{data_source}/#{d['data']}.json"
+        if d['cache'] && File.exists?(path)
+          source = JSON.load(File.open(path))
+        else
           source = JSON.load(open(d['json']))
-          if target
-            HashJoiner.deep_merge target, source
-          else
-            site.data[d['data']] = source
+        end
+        site.data[d['data']] = source
+        if d['cache']
+          open(path, 'wb') do |file|
+            file << JSON.generate(site.data[d['data']])
           end
-          if d['cache']
-            data_source = (site.config['data_source'] || '_data')
-            path = "#{data_source}/#{d['data']}.json"
-            open(path, 'wb') do |file|
-              file << JSON.generate(site.data[d['data']])
-            end
-          end
-        rescue
-          next
         end
       end
     end
